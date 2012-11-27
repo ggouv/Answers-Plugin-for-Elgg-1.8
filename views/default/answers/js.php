0a423f6af21e981b27eaf9e9e4afc6af82f2aa40
ManUtopiK <emmanuel.salomon@free.fr>
2012-11-27 17:37:29 +0100

    Fix js

--- views/default/answers/js.php
+++ views/default/answers/js.php
@@ -1,19 +1,18 @@
 //<script>
 elgg.provide('elgg.answers');
 
-var elgg.answers.init = function() {
+elgg.answers.init = function() {
 
 	// ajaxified action for vote up
 	$('.answers_like').live('click', function() {
-		if ($.data(this, 'clicked') || $(this).hasClass('checked')) // Prevent double-click
+		if ($.data(this, 'clicked') || $(this).hasClass('checked')) { // Prevent double-click
 			return false;
-		else {
+		} else {
 			$.data(this, 'clicked', true);
-
 			elgg.action('answer/like', {
 				data: {
-					answer_id: $('answer_id').val();
-				}
+					answer_id: $('answer_id').val()
+				},
 				success: function(json) {
 
 					if ($('.answers_like').hasClass('answers_like_selected')) {
@@ -27,37 +26,32 @@ var elgg.answers.init = function() {
 					
 					$.data(thisVote, 'clicked', false);
 				}
-			}
+			});
 		}
-	}
+	});
 
 	// ajaxified action for vote down
 	$('.answers_dislike').live('click', function() {
-		if ($.data(this, 'clicked') || $(this).hasClass('checked')) // Prevent double-click
+		if ($.data(this, 'clicked') || $(this).hasClass('checked')) { // Prevent double-click
 			return false;
-		else {
+		} else {
 			$.data(this, 'clicked', true);
-
 			elgg.action('answer/dislike', {
 				data: {
-					answer_id: $('answer_id').val();
-				}
+					answer_id: $('answer_id').val()
+				},
 				success: function(json) {
-
 					if ($('.answers_dislike').hasClass('answers_dislike_selected')) {
 						$('.answers_dislike').removeClass('answers_dislike_selected');
 						$('.answers_rating').val($('.answers_rating').val() + 1);
-					}
-					else {
+					} else {
 						$('.answers_dislike').addClass('answers_dislike_selected');
 						$('.answers_rating').val($('.answers_rating').val() - 1);
 					}
-					
 					$.data(thisVote, 'clicked', false);
 				}
-			}
+			});
 		}
-	}
-};
-
+	});
+}
 elgg.register_hook_handler('init', 'system', elgg.answers.init);
