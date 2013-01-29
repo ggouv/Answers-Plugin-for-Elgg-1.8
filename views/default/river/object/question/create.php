e3bf1b09cd7e9101a3bf1294c8d8d5ed28cc256b
ManUtopiK <emmanuel.salomon@free.fr>
2013-01-29 16:44:05 +0100

    Don't need a special river view when create a question

--- views/default/river/object/question/create.php
+++ views/default/river/object/question/create.php
@@ -3,37 +3,11 @@
  * Add question river view
  */
 
-$subject = $vars['item']->getSubjectEntity();
 $object = $vars['item']->getObjectEntity();
-$container = $object->getContainerEntity();
-
-$subject_link = elgg_view('output/url', array(
-	'href' => $subject->getURL(),
-	'text' => $subject->name,
-	'class' => 'elgg-river-subject',
-	'is_trusted' => true,
-));
-
-$object_link = elgg_view('output/url', array(
-	'href' => $object->getURL(),
-	'text' => $object->title,
-	'class' => 'elgg-river-object',
-	'is_trusted' => true,
-));
-
-$group_link = elgg_view('output/url', array(
-	'href' => $container->getURL(),
-	'text' => $container->name,
-	'is_trusted' => true,
-));
-$group_string = elgg_echo('river:ingroup', array($group_link));
-
-$excerpt = strip_tags(elgg_get_excerpt($object->description, 100));
-
-$summary = elgg_echo("question:river:created", array($subject_link, $object_link, $group_string));
+$excerpt = strip_tags($object->description);
+$excerpt = elgg_get_excerpt($excerpt, 140);
 
 echo elgg_view('river/elements/layout', array(
-	'item' => $item,
-	'summary' => $summary,
+	'item' => $vars['item'],
 	'message' => $excerpt,
 ));
--- views/json/river/object/question/create.php
+++ views/json/river/object/question/create.php
@@ -2,39 +2,13 @@
 /**
  * Add question river view
  */
-
 global $jsonexport;
 
-$subject = $vars['item']->getSubjectEntity();
 $object = $vars['item']->getObjectEntity();
-$container = $object->getContainerEntity();
+$excerpt = strip_tags($object->description);
+$excerpt = elgg_get_excerpt($excerpt, 140);
 
-$subject_link = elgg_view('output/url', array(
-	'href' => $subject->getURL(),
-	'text' => $subject->name,
-	'class' => 'elgg-river-subject',
-	'is_trusted' => true,
-));
-
-$object_link = elgg_view('output/url', array(
-	'href' => $object->getURL(),
-	'text' => $object->title,
-	'class' => 'elgg-river-object',
-	'is_trusted' => true,
-));
-
-$group_link = elgg_view('output/url', array(
-	'href' => $container->getURL(),
-	'text' => $container->name,
-	'is_trusted' => true,
-));
-$group_string = elgg_echo('river:ingroup', array($group_link));
-
-$excerpt = strip_tags(elgg_get_excerpt($object->description, 100));
-
-$summary = elgg_echo("question:river:created", array($subject_link, $object_link, $group_string));
-
-$vars['item']->summary = $summary;
+$vars['item']->summary = elgg_view('river/elements/summary', array('item' => $vars['item']), FALSE, FALSE, 'default');
 $vars['item']->message = $excerpt;
 
 $jsonexport['activity'][] = $vars['item'];
